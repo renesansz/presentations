cdd682229a44ce4b7471e149b6b630090cd77d5a
Renemari Padillo <padillo.renemari@gmail.com>
2015-09-07 01:42:51 +0900

    [CHORE] Added slides directive.

--- angular-web-applications/src/scripts/angular.app.js
+++ angular-web-applications/src/scripts/angular.app.js
@@ -33,23 +33,58 @@
 
     'use strict';
 
-    angular.module('app').directive('slides', slides);
+    angular.module('app')
+           .directive('slides', slides)
+           .directive('slideItem', slideItem);
 
     function slides(NG_PATH) {
 
+        _Controller.$inject = ['$scope'];
         return {
-            restrict: 'EA',
-            templateUrl: NG_PATH + 'view.html',
+            restrict: 'E',
+            scope: {},
+            transclude: true,
+            templateUrl: NG_PATH.DIRECTIVE + 'slides.view.html',
+            controller: _Controller
+        };
+
+        /**
+         * Directive Controller
+         */
+        function _Controller($scope) {
+
+            $scope.slides = [];
+
+            this.AddSlide = function (newSlide) {
+                $scope.slides.push(newSlide);
+            };
+
+        }
+
+    }
+    slides.$inject = ['NG_PATH'];
+
+    function slideItem(NG_PATH) {
+
+        return {
+            require: '^slides',
+            restrict: 'E',
+            transclude: true,
+            scope: {
+                title: '=title'
+            },
+            templateUrl: NG_PATH.DIRECTIVE + 'slideItem.view.html',
             link: _Link
         };
 
         /**
          * Directive Controller
          */
-        function _Link() {
+        function _Link(scope, element, attrs, slides) {
+            slides.AddSlide(scope);
         }
 
     }
-    slides.$inject = ['NG_PATH'];
+    slideItem.$inject = ['NG_PATH'];
 
 }());
