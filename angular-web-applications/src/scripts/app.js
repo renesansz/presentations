97098f8ef1b2108add08e593b72f1b594a6b5b60
Renemari Padillo <padillo.renemari@gmail.com>
2015-09-12 12:24:06 +0900

    Updated presentation to use native JavaScript app.

---
+++ angular-web-applications/src/scripts/app.js
@@ -0,0 +1,65 @@
+(function () {
+
+    // Set to true to show console.log for debugging
+    var DEBUG_MODE = true;
+
+    'use strict';
+
+    var Slide = function (title) {
+
+        // Constructor
+        this.title = title;
+        this.eleRef = null;
+
+    };
+
+    Slide.prototype.SetReference = function (dom) {
+        this.eleRef = dom;
+    };
+
+    Slide.prototype.MoveLeft = function () {};
+    Slide.prototype.MoveRight = function () {};
+    Slide.prototype.TransitionLeft = function () {};
+    Slide.prototype.TransitionRight = function () {};
+
+    /////////////////
+    /// APP START ///
+    /////////////////
+
+    var slides = [];
+    var currentSlideIdx = 0;
+
+    /**
+     * Initialize App
+     */
+    function Initialize() {
+        
+        GetSlides();
+
+    }
+
+    /**
+     * Get slide DOM reference
+     */
+    function GetSlides() {
+
+        var slidesDOM = document.getElementsByClassName('slide');
+
+        setTimeout(function () {
+
+            for (var i = 0, limit = slidesDOM.length; i < limit; ++i) {
+                
+                var newSlide = new Slide(slidesDOM[i].dataset.title);
+                    newSlide.SetReference(slidesDOM[i]);
+
+                slides.push(newSlide);
+
+            }
+
+        }, 0);
+
+    }
+
+    Initialize();
+
+}());
